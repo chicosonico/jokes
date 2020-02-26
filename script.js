1dfdac96c022b49167ed76a1159da941a471ac4c
Pablo <pablonet@Pablos-MacBook-Pro.local>
2020-02-26 11:45:39 +0100

    working. TODO: CSS styles

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 
 
-$(function() {
+
+
+    $("#jokeCall").click(function() {
+        getJoke();
+    });
+    
+    function getJoke(){
+
         var id = Math.floor(Math.random()*(30+1));
         console.log(id);
 
         var $htmlJoke = $('#update');
+        
         $.ajax({
             type: "GET",
             url: "http://api.icndb.com/jokes/random",
             success: function(data) {
                 console.log("Success " + data.value.joke);
-                $htmlJoke.append(data.value.joke);
+                $htmlJoke.text(data.value.joke);
             },
             error: function(){
                 alert("json not found");
             }
-        });
-             
-    });
+        }); 
+    };
+
+  
+
+
+
+
+
     
 
